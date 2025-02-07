d35ddcfce11a52d25bd3468cc2f9eb1c76c40b47
Mohammad <mohammad.kamran@sas.com>
2025-02-07 11:51:10 -0500

    updated ami in main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,12 @@ provider "aws" {
   region = "us-east-1"
 }
 
+
 resource "aws_instance" "example" {
   ami           = "ami-085ad6ae776d8f09c"  # Amazon Linux 2023 AMI
   instance_type = "t2.micro"
-
+  subnet_id     = subnet-092bf86693f3d649c
+  vpc_security_group_ids = [sg-07679cd0804dd607b]
   tags = {
     Name = "GitHubActionsEC2"
   }
