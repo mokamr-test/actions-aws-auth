03bfffb6be79a8f4450b38f9b3a5cfb5aebe65c8
Mohammad <mohammad.kamran@sas.com>
2025-02-07 11:41:49 -0500

    updated ami in main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI
+  ami           = "ami-085ad6ae776d8f09c"  # Amazon Linux 2023 AMI
   instance_type = "t2.micro"
 
   tags = {
